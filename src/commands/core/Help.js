e3bf08df433a2077b3a0afcda65b96376031a873
‧₊˚ ⋅♡𝐉𝐅𝐋𝐄𝐗 𝐎𝐆 ࣪ ִֶָ☾ <kakamfaume@gmail.com>
2024-07-07 08:50:10 +0300

    Update Help.js

--- src/commands/core/Help.js
+++ src/commands/core/Help.js
@@ -1,79 +1,85 @@
-import BaseCommand from '../../libs/BaseCommand.js'
+import BaseCommand from '../../libs/BaseCommand.js';
 
 export default class Command extends BaseCommand {
     constructor(client, handler) {
         super(client, handler, {
             command: 'help',
             aliases: ['menu', 'h'],
-            category: 'core',
+            category: 'core',  // Updated category
             description: {
                 content: 'Displays the menu',
                 usage: '[command]'
             },
-            dm: true,
+            dm: false,
             exp: 1
-        })
+        });
     }
 
     exec = async (M, parsedArgs) => {
         if (!parsedArgs.text) {
-            const commands = this.handler.commands.keys()
-            const categories = {}
+            const commands = this.handler.commands.keys();
+            const categories = {};
             for (const command of commands) {
-                const info = this.handler.commands.get(command)
-                if (!command) continue
-                if (!info?.config?.category || info.config.category === 'dev') continue
-                if (Object.keys(categories).includes(info.config.category)) categories[info.config.category].push(info)
+                const info = this.handler.commands.get(command);
+                if (!command) continue;
+                if (Object.keys(categories).includes(info.config.category)) categories[info.config.category].push(info);
                 else {
-                    categories[info.config.category] = []
-                    categories[info.config.category].push(info)
+                    categories[info.config.category] = [];
+                    categories[info.config.category].push(info);
                 }
             }
-            let text = `👋🏻 (❤️ω❤️) Konnichiwa, senpai ${M.sender.username}! this is ${this.client.util.capitalize(this.client.config.name)}\n\n🎋 *Support us by following us on instagram:* https://www.instagram.com/das_abae\n\n💡 My Prefix is *( ${this.client.config.prefix} )*\n\n    ⇓ *📪 Command list 📪* ⇓\n\n`
-            const keys = Object.keys(categories)
-            for (const key of keys)
-                text += `┌ ◦ *${this.emojis[keys.indexOf(key)]}「${key.toUpperCase()}」${this.emojis[keys.indexOf(key)]}*\n${categories[
-                    key
-                ]
-                    .map(
-                        (command) =>
-                            `*${this.client.config.prefix}${this.replaceWithCustomAlphabet(command.config?.command)}* _${command.config.description.usage ?? ''}_`
-                    )
-                    .join('\n')}\n\n`
+            let text = `╭─「 (づ￣ ³￣)づ 」*\n*│ ɴᴀᴍᴇ:* ${this.replaceWithCustomAlphabet(this.client.config.name)}\n*│ ᴜsᴇʀ: @⁨${this.replaceWithCustomAlphabet(M.sender.username)}*\n*│ ᴘʀᴇғɪx:* [ ${this.client.config.prefix} ]\n*│ ᴏᴡɴᴇʀ:* *${this.client.util.capitalize(this.replaceWithCustomAlphabet('𝐉𝐅𝐋𝐄𝐗 𝐎𝐆 {}'))}*\n*╰────────────┈平和* \n\n📚 *Below are my available commands*:\n\n`;
+            const keys = Object.keys(categories);
+            const categoryEmojis = this.getCategoryEmojis();
 
-            return void (await M.replyRaw({
-                image: {
-                    url: 'https://i.pinimg.com/originals/82/83/68/8283687daf61e2464f30537d2cbca205.jpg'
-                },
-                caption: `${text}*📇 Notes:*\n*➪ Use ${this.client.config.prefix}help <command name> from help the list to see its description and usage*\n*➪ Eg: ${this.client.config.prefix}help profile*\n*➪ <> means required and [ ] means optional, don't include <> or [ ] when using command.*`,
-                contextInfo: {
-                    externalAdReply: {
-                        title: `${this.client.util.capitalize(this.client.config.name)}'s Commands`,
-                        body: '',
-                        thumbnail: await this.client.util.fetchBuffer('https://i.imgur.com/22WppSh.jpg'),
-                        mediaType: 1,
-                        mediaUrl: '',
-                        sourceUrl: '',
-                        ShowAdAttribution: true
-                    }
-                }
-            }))
+            for (const key of keys) {
+                text += `*${this.replaceWithCustomAlphabet(key.replace('undefined ', ''))} ${categoryEmojis[key]} :-*\n`;
+                text += `➮\`\`\`${categories[key].map(command => command.config.command).join(' , ')}\`\`\`\n\n`;
+            }
+
+            // Adding evil emoji reaction
+            await M.react('😈');
+
+            text += `🗒️ *Note:* To summon more information about a command, use ${this.client.config.prefix}help <cmd_name>. Example: *${this.client.config.prefix}help info*\n\n*> ©️𝐆𝐄𝐓𝐎-𝐁𝐎𝐓 😈 Inc*`;
+
+            return void (await M.reply(text));
         }
-        const key = parsedArgs.text.toLowerCase()
-        const command = this.handler.commands.get(key) || this.handler.aliases.get(key)
-        if (!command) return void (await M.reply(`❌ No Command of Alias Found *"${key}"*`))
+        const key = parsedArgs.text.toLowerCase();
+        const command = this.handler.commands.get(key) || this.handler.aliases.get(key);
+        if (!command) return void (await M.reply(`❌ No Command or Alias Found *"${key}"*`));
+
+        // Get the command's status and cooldown
         const cmdStatus = (await this.client.DB.command.get(command.config?.command)) ?? {
             isDisabled: false,
             reason: ''
-        }
-        return void (await M.reply(`🟥 *Command: ${command.config.command}*
-🟧 *Category: ${command.config.category}*
-🟨 *Aliases: ${command.config.aliases ? command.config.aliases.join(', ').trim() : 'None'}*
-🟩 *PrivateChat: ${command.config.dm ? 'True' : 'False'}*
-🟦 *Admin: ${command.config.adminOnly ? 'True' : 'False'}*
-⬛ *Status: ${cmdStatus.isDisabled}* - ${cmdStatus.reason}
-🟪 *Usage: ${this.client.config.prefix}${command.config.command} ${command.config.description.usage ?? ''}*
-⬜ *Description: ${command.config.description?.content}*`))
+        };
+        const cmdCooldown = command.config.cooldown ? `${command.config.cooldown} seconds` : 'None';
+
+        // Construct the details message
+        const detailsMessage = `☠ *Command:* ${this.replaceWithCustomAlphabet(command.config.command)}
+🎴 *Aliases:* ${command.config.aliases ? command.config.aliases.join(', ').trim() : 'None'}
+🔗 *Category:* ${this.replaceWithCustomAlphabet(command.config.category)}
+⏰ *Cooldown:* ${cmdCooldown}
+🎗 *Usage:* ${this.client.config.prefix}${command.config.command} ${command.config.description.usage ?? ''}
+🧧 *Description:* ${this.replaceWithCustomAlphabet(command.config.description?.content)}`;
+
+        return void (await M.reply(detailsMessage));
+    }
+
+    getCategoryEmojis = () => {
+        return {
+            'core': '⚙️',
+            'fun': '🎭',
+            'game': '🎮',
+            'media': '🎵',
+            'moderation': '🔨',
+            'search': '🔍',
+            'utils': '🔧',
+            'weeb': '👾',
+            'cards': '🃏',
+            'economy': '💰',  // Added economy emoji
+            'dev': '🕵🏽‍♂️' // Added dev emoji
+        };
     }
 
     replaceWithCustomAlphabet = (sentence) => {
@@ -104,18 +110,16 @@ export default class Command extends BaseCommand {
             x: 'x',
             y: 'ʏ',
             z: 'ᴢ'
-        }
-        const words = sentence.split(' ')
+        };
+        const words = sentence.split(' ');
         const replacedWords = words.map((word) => {
-            const letters = word.split('')
+            const letters = word.split('');
             const replacedLetters = letters.map((letter) => {
-                const lowercaseLetter = letter.toLowerCase()
-                return customAlphabetMap[lowercaseLetter] || letter
-            })
-            return replacedLetters.join('')
-        })
-        return replacedWords.join(' ')
+                const lowercaseLetter = letter.toLowerCase();
+                return customAlphabetMap[lowercaseLetter] || letter;
+            });
+            return replacedLetters.join('');
+        });
+        return replacedWords.join(' ');
     }
-
-    emojis = ['🌀', '🎴', '🔮', '👑', '🎈', '⚙️', '🍀', '💈', '🔰']
-}
+                }
